241a8dea393b6a187c3235560c969ba27897b230
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-08-26 18:11:41 +0530

    t change

--- Source/Config/Colors.ts
+++ Source/Config/Colors.ts
@@ -2,7 +2,7 @@ export default {
   primary: "",
   secondary: "",
   white: "#fff",
-  Transparent_White: "#d1d1d1",
+  transparent_White: "#d1d1d1",
   black: "#000",
   default_red: "red",
   default_grey: "grey",
