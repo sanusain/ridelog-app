7a747b7feaed9fcf9b4912b7cc482c5681e3e71c
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-08-28 16:59:01 +0530

    background color tint fixed

--- Source/Screens/Auth/signup.tsx
+++ Source/Screens/Auth/signup.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { View } from "react-native"
 import { ScrollView } from "react-native-gesture-handler"
 import LightTextInput from "../../Components/LightTextInput"
 import SquareButton from "../../Components/SquareButton"
@@ -25,120 +26,125 @@ const SignUp: React.FunctionComponent<Props> = (props) => {
   }
 
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      style={{
-        flex: 1,
-        marginHorizontal: 20,
-        marginTop: 40,
-        backgroundColor: Colors.white,
-      }}
-    >
-      <TextMontserrat type={"semibold"} fontSize={30}>
-        Create Account
-      </TextMontserrat>
-      <TextOpenSans fontSize={16} style={{ marginTop: 10, textAlign: "left" }}>
-        Enter your Name, Email and Password for sign up.
+    <View style={{ flex: 1, backgroundColor: Colors.white }}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{
+          flex: 1,
+          marginHorizontal: 20,
+          marginTop: 40,
+          backgroundColor: Colors.white,
+        }}
+      >
+        <TextMontserrat type={"semibold"} fontSize={30}>
+          Create Account
+        </TextMontserrat>
         <TextOpenSans
           fontSize={16}
-          style={{ color: Colors.imperialRed }}
-          onPress={handleSignInRedirection}
+          style={{ marginTop: 10, textAlign: "left" }}
+        >
+          Enter your Name, Email and Password for sign up.
+          <TextOpenSans
+            fontSize={16}
+            style={{ color: Colors.imperialRed }}
+            onPress={handleSignInRedirection}
+          >
+            {" "}
+            Already have account?
+          </TextOpenSans>
+        </TextOpenSans>
+
+        <LightTextInput
+          placeholder={"Call Sign"}
+          textContentType={"givenName"}
+          style={{
+            alignSelf: "center",
+            marginTop: 20,
+            width: "100%",
+          }}
+          onChangeText={(text) => {
+            console.log(text)
+          }}
+        />
+        <LightTextInput
+          placeholder={"Email Address"}
+          textContentType={"emailAddress"}
+          style={{
+            alignSelf: "center",
+            marginTop: 20,
+            width: "100%",
+          }}
+          onChangeText={(text) => {
+            console.log(text)
+          }}
+        />
+        <LightTextInput
+          placeholder={"Password"}
+          textContentType={"password"}
+          style={{
+            alignSelf: "center",
+            marginTop: 20,
+            width: "100%",
+          }}
+          onChangeText={(text) => {
+            console.log(text)
+          }}
+        />
+        <LightTextInput
+          placeholder={"Confirm Password"}
+          textContentType={"password"}
+          style={{
+            alignSelf: "center",
+            marginTop: 20,
+            width: "100%",
+          }}
+          onChangeText={(text) => {
+            console.log(text)
+          }}
+        />
+
+        <SquareButton
+          title={"SIGN UP"}
+          buttonBackgroundColor={Colors.imperialRed}
+          style={{ width: "100%", marginTop: 20 }}
+          onPress={handleSignUp}
+        />
+
+        <TextMontserrat
+          fontSize={16}
+          style={{
+            textAlign: "center",
+            marginVertical: 20,
+          }}
         >
           {" "}
-          Already have account?
-        </TextOpenSans>
-      </TextOpenSans>
+          OR
+        </TextMontserrat>
 
-      <LightTextInput
-        placeholder={"Call Sign"}
-        textContentType={"givenName"}
-        style={{
-          alignSelf: "center",
-          marginTop: 20,
-          width: "100%",
-        }}
-        onChangeText={(text) => {
-          console.log(text)
-        }}
-      />
-      <LightTextInput
-        placeholder={"Email Address"}
-        textContentType={"emailAddress"}
-        style={{
-          alignSelf: "center",
-          marginTop: 20,
-          width: "100%",
-        }}
-        onChangeText={(text) => {
-          console.log(text)
-        }}
-      />
-      <LightTextInput
-        placeholder={"Password"}
-        textContentType={"password"}
-        style={{
-          alignSelf: "center",
-          marginTop: 20,
-          width: "100%",
-        }}
-        onChangeText={(text) => {
-          console.log(text)
-        }}
-      />
-      <LightTextInput
-        placeholder={"Confirm Password"}
-        textContentType={"password"}
-        style={{
-          alignSelf: "center",
-          marginTop: 20,
-          width: "100%",
-        }}
-        onChangeText={(text) => {
-          console.log(text)
-        }}
-      />
-
-      <SquareButton
-        title={"SIGN UP"}
-        buttonBackgroundColor={Colors.imperialRed}
-        style={{ width: "100%", marginTop: 20 }}
-        onPress={handleSignUp}
-      />
-
-      <TextMontserrat
-        fontSize={16}
-        style={{
-          textAlign: "center",
-          marginVertical: 20,
-        }}
-      >
-        {" "}
-        OR
-      </TextMontserrat>
-
-      <SquareButton
-        title={"CONNECT WITH GOOGLE"}
-        onPress={handleSignUpWithGoogle}
-        style={{ width: "100%" }}
-      />
-      <SquareButton
-        title={"CONNECT WITH FACEBOOK"}
-        onPress={handleSignUpWithFacebook}
-        buttonBackgroundColor={Colors.facebookBlue}
-        style={{ marginTop: 20, width: "100%" }}
-      />
-      <TextMontserrat
-        fontSize={12}
-        style={{
-          textAlign: "center",
-          marginVertical: 10,
-          color: Colors.tGrey,
-          opacity: 0.67,
-        }}
-      >
-        By Signing up you agree to our Terms Conditions & Privacy Policy.
-      </TextMontserrat>
-    </ScrollView>
+        <SquareButton
+          title={"CONNECT WITH GOOGLE"}
+          onPress={handleSignUpWithGoogle}
+          style={{ width: "100%" }}
+        />
+        <SquareButton
+          title={"CONNECT WITH FACEBOOK"}
+          onPress={handleSignUpWithFacebook}
+          buttonBackgroundColor={Colors.facebookBlue}
+          style={{ marginTop: 20, width: "100%" }}
+        />
+        <TextMontserrat
+          fontSize={12}
+          style={{
+            textAlign: "center",
+            marginVertical: 10,
+            color: Colors.tGrey,
+            opacity: 0.67,
+          }}
+        >
+          By Signing up you agree to our Terms Conditions & Privacy Policy.
+        </TextMontserrat>
+      </ScrollView>
+    </View>
   )
 }
 
