e705d8cb0b35fc76f25797ea5500e7c75a740bb7
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-08-30 11:25:47 +0530

    install firebase, save config 2/2

---
+++ Source/Config/firebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from "firebase"
+
+// Initialize Firebase
+const firebaseConfig = {
+  API_KEY: "AIzaSyCTZHtr_rE80f9G3QwI8gTB8JNHj69xZTM",
+  AUTH_DOMAIN: "https://ridelogg.firebaseio.com",
+  DATABASE_URL: "https://ridelogg.firebaseio.com",
+  PROJECT_ID: "ridelogg",
+  STORAGE_BUCKET: "ridelogg.appspot.com",
+  APP_ID: "1:931489511293:android:da3edce7bac9b418befe14",
+}
+
+firebase.initializeApp(firebaseConfig)
