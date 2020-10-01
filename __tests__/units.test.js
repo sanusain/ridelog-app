8cebab19b242fb3bd26d1d4255000cc2754f2321
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-10-02 03:33:47 +0530

    initialised unit tests

---
+++ __tests__/units.test.js
@@ -0,0 +1,11 @@
+// tests for all functions in the app
+
+//#######################  DASHBOARD
+
+//#######################  FUEL
+
+//#######################  UN ASSIGNED
+
+//#######################  SERVICE LOG
+
+//#######################  ACCOUNTS
