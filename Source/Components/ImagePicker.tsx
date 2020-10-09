239ad0e2699e23a640f299105832e13f9cc721bc
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-10-09 19:13:31 +0530

    delete partially works

--- Source/Components/ImagePicker.tsx
+++ Source/Components/ImagePicker.tsx
@@ -1,10 +1,13 @@
 import { EvilIcons } from "@expo/vector-icons"
-import React, { FunctionComponent } from "react"
+import { BlurView } from "expo-blur"
+import React, { FunctionComponent, useState } from "react"
 import { Image, TouchableOpacity, View } from "react-native"
+import uuid from "react-native-uuid"
 import { connect } from "react-redux"
 import Colors from "../Config/Colors"
 import { ImageSpecs } from "../Screens/Refuel/types"
 import { AppState, dispatchHandler } from "../State-management"
+import TextMontserrat from "./TextMontserrat"
 
 type Props = {
   refuelLogImages: Array<ImageSpecs>
@@ -14,31 +17,103 @@ type Props = {
 }
 
 const CustomImagePicker: FunctionComponent<Props> = (props) => {
+  const [deleteBlur, setDeleteBlur] = useState(false)
+
   return (
     <View style={{ flex: 1 }}>
       {props.refuelLogImages.length !== 0 &&
         props.refuelLogImages.map((image) => (
-          <TouchableOpacity
-            key={image.uid}
-            onPress={() => {
-              props.handleImagePress(image)
-            }}
-          >
-            <Image
-              source={{ uri: image.uri }}
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                height: 120,
-                marginTop: 5,
-                marginBottom: 5,
-                marginHorizontal: 20,
-                borderRadius: 7,
-                borderWidth: 1,
-                resizeMode: "cover",
+          <>
+            <TouchableOpacity
+              key={image.uid}
+              onPress={() => {
+                setDeleteBlur(true)
               }}
-            />
-          </TouchableOpacity>
+            >
+              <Image
+                source={{ uri: image.uri }}
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: 120,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  marginHorizontal: 20,
+                  borderRadius: 7,
+                  borderWidth: 1,
+                  resizeMode: "cover",
+                }}
+              />
+            </TouchableOpacity>
+            {deleteBlur ? ( //this is the delete overlay
+              <BlurView
+                key={uuid.v4()}
+                intensity={100}
+                style={{
+                  backgroundColor: Colors.white,
+                  position: "absolute",
+                  width: "90%",
+                  height: 120,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  marginHorizontal: 20,
+                  borderRadius: 7,
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "row",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    marginHorizontal: 20,
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: Colors.redLite,
+                      borderRadius: 7,
+                      padding: 7,
+                    }}
+                    onPress={() => {
+                      setDeleteBlur(false)
+                      props.handleImagePress(image)
+                      console.log("pressed delete")
+                    }}
+                  >
+                    <TextMontserrat
+                      fontSize={16}
+                      weight={"semibold"}
+                      fontColor={Colors.white}
+                      style={{ letterSpacing: 0.7 }}
+                    >
+                      Delete
+                    </TextMontserrat>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: Colors.redLite,
+                      borderRadius: 7,
+                      padding: 7,
+                    }}
+                    onPress={() => {
+                      setDeleteBlur(false)
+                      console.log("pressed cancel")
+                    }}
+                  >
+                    <TextMontserrat
+                      fontSize={16}
+                      weight={"semibold"}
+                      fontColor={Colors.white}
+                      style={{ letterSpacing: 0.7 }}
+                    >
+                      Cancel
+                    </TextMontserrat>
+                  </TouchableOpacity>
+                </View>
+              </BlurView>
+            ) : null}
+          </>
         ))}
 
       {props.refuelLogImages.length < 2 && (
