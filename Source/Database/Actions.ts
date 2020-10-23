a6dc1bb018096e1d6b80d15e83343cd0bac57119
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-10-23 19:02:54 +0530

    background jobs redesign

--- Source/Database/Actions.ts
+++ Source/Database/Actions.ts
@@ -2,25 +2,11 @@ import { RefuelData, vehicleInfo } from "../Screens/Dashboard/types"
 import { AppState } from "../State-management"
 import { Action } from "../State-management/root-action"
 
-export class ActionSetVehicles extends Action<vehicleInfo> {
+export class ActionSetSelectedVehicle extends Action<vehicleInfo> {
   updateState(state: AppState) {
-    if (this.payload)
-      if (
-        state.vehiclesInfo.findIndex(
-          (value) => JSON.stringify(value) === JSON.stringify(this.payload)
-        ) === -1
-      )
-        state.vehiclesInfo.push(this.payload)
-  }
-}
-
-export class ActionRemoveVehicle extends Action<string> {
-  updateState(state: AppState) {
-    state.vehiclesInfo.forEach((item, index) => {
-      if (item.vcallsign === this.payload) {
-        state.vehiclesInfo.splice(index, 1)
-      }
-    })
+    if (this.payload) {
+      state.selectedVehicle = this.payload
+    }
   }
 }
 
