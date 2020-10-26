7dc2fbbec4f8ab1ff36a67a05bc08f5a24ff32e5
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-10-26 23:56:43 +0530

    cleared test

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -1,22 +1,3 @@
-import { ActionSetVehicles } from "../Source/Database/Actions"
-
 // creating the sameobject type as of the class
 // update the payload of the object with testpayload data
 // call the action with with testpayload data, both should match.
-
-describe("Action", () => {
-  it("Set Vehicles", () => {
-    const testData = {
-      vcallsign: "Ghost",
-      maker: "Rolls",
-      model: "Royce",
-      odo: "5000",
-      plate: "AGNO23",
-      vin: "ALASKA234SF234",
-      year: 2024,
-    }
-    const expectedData = new ActionSetVehicles()
-    expectedData.payload = testData
-    expect(new ActionSetVehicles(testData)).toEqual(expectedData)
-  })
-})
