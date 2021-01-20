cb514567baf07c1a85de78a5c69179217e30fca2
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2021-01-21 00:42:58 +0530

    added services api and uploadtypes

--- Source/api/service.ts
+++ Source/api/service.ts
@@ -1,18 +1,18 @@
 import Axios from 'axios'
 import {getServer} from '../Config'
 import {getAuthToken} from '../Contexts/AuthProvider'
-import {RefuelLog} from '../Types'
+import {ServiceLog} from '../Types'
 
 const BASE_URL = getServer()
 
-export async function addCloudRefuelLog(log: RefuelLog): Promise<any> {
+export async function addCloudServiceLog(log: ServiceLog): Promise<any> {
   const uploadableLog: any = {
     _id: log._id,
     vehicleId: log.vehicleId,
     odo: log.odo,
-    quantity: log.quantity,
     date: log.date,
-    unitCost: log.unitCost,
+    serviceCount: log.serviceCount,
+    notes: log.notes,
     totalCost: log.totalCost,
     location: log.location,
     images: log.images,
@@ -21,29 +21,29 @@ export async function addCloudRefuelLog(log: RefuelLog): Promise<any> {
     const authToken = getAuthToken()
     if (!authToken) throw new Error('authToken Missing!')
 
-    const URL = `${BASE_URL}/api/refuellog`
+    const URL = `${BASE_URL}/api/servicelog`
     const result = await Axios.post(URL, uploadableLog, {
       headers: {'x-auth-token': authToken},
     })
 
     if (result.status !== 201) throw new Error('201_NOT_CREATED')
-    return console.info('REFUEL_LOG_UPLOADED')
+    return console.info('SERVICE_LOG_UPLOADED')
   } catch (error) {
-    return console.info('ERROR_IN_addCloudRefuelLog', error.response.data)
+    return console.info('ERROR_IN_addCloudServiceLog', error.response.data)
   }
 }
 
-export async function removeCloudRefuelLog(log: RefuelLog): Promise<any> {
+export async function removeCloudServiceLog(log: ServiceLog): Promise<any> {
   try {
     const authToken = getAuthToken()
     if (!authToken) throw new Error('authToken Missing!')
-    const URL = `${BASE_URL}/api/refuellog/:${log.vehicleId}/:${log._id}`
+    const URL = `${BASE_URL}/api/servicelog/:${log.vehicleId}/:${log._id}`
     const result = await Axios.delete(URL, {
       headers: {'x-auth-token': authToken},
     })
     if (result.status !== 200) throw new Error('201_NOT_CREATED')
-    return console.info('REMOVED_REFUEL_LOG_FROM_SERVER')
+    return console.info('REMOVED_SERVICE_LOG_FROM_SERVER')
   } catch (error) {
-    return console.info('ERROR_IN_removeCloudRefuelLog', error.message)
+    return console.info('ERROR_IN_removeCloudServiceLog', error.message)
   }
 }
