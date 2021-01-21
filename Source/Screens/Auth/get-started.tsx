d716b8efa158ba926db53eebb67e0d053de55984
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2021-01-21 14:18:38 +0530

    added support for lottieanimations, onboarding

--- Source/Screens/Auth/get-started.tsx
+++ Source/Screens/Auth/get-started.tsx
@@ -1,30 +1,64 @@
 import React from 'react'
 import {View} from 'react-native'
+import Onboarding from 'react-native-onboarding-swiper'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import SquareButton from '../../Components/SquareButton'
 import Colors from '../../Config/Colors'
 
-type Props = {navigation?: any}
+type Props = {navigation: any}
 
 const GetStarted: React.FunctionComponent<Props> = (props: Props) => {
   const handleGetstarted = () => {
     console.log('handle getstarted')
     props.navigation.navigate('signIn')
   }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        marginBottom: 25,
-        backgroundColor: Colors.white,
-      }}>
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <Onboarding
+        bottomBarHighlight={false}
+        showSkip={false}
+        showNext={false}
+        showDone={false}
+        pages={[
+          {
+            backgroundColor: '#fff',
+            image: (
+              <MaterialCommunityIcons
+                name="clipboard-check-multiple-outline"
+                size={150}
+                color={Colors.spaceCadetPurple}
+                style={{opacity: 0.8}}
+              />
+            ),
+            title: 'Ridelogg',
+            subtitle:
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            subTitleStyles: {color: Colors.default_grey},
+          },
+          {
+            backgroundColor: '#fff',
+            image: (
+              <FontAwesome5
+                name="file-invoice-dollar"
+                size={150}
+                color={Colors.spaceCadetPurple}
+                style={{opacity: 0.8}}
+              />
+            ),
+            title: 'Placeholder',
+            subtitle:
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            subTitleStyles: {color: Colors.default_grey},
+          },
+        ]}
+      />
       <SquareButton
         title="GET STARTED"
-        onPress={handleGetstarted}
-        style={{paddingHorizontal: 90}}
-        borderRadius={8}
+        style={{alignSelf: 'center', marginBottom: 20}}
         buttonBackgroundColor={Colors.imperialRed}
+        onPress={handleGetstarted}
       />
     </View>
   )
